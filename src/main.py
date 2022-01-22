3c2d28da69a34370ba4bb32a212e12dec94bc6e6
Koala-Development <koala@mail.co.uk>
2022-01-22 16:54:13 +0000

    Created bot, added user level system.

---
+++ src/main.py
@@ -0,0 +1,25 @@
+import discord
+from flask import session
+from models import User, engine, Base, Session
+
+Base.metadata.create_all(engine)
+Session = Session()
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        author = message.author
+        user = User.query.filter_by(user=author).first()
+        if user:
+            user.user_lvl =+ 5
+            if message.content.startswith('&&lvl'):
+                await message.reply("Your level is: " + str(user.user_lvl))
+        else:
+            new_user = User(user=author, author_lvl=5)
+            Session.add(new_user)
+            Session.commit()
+
+client = MyClient()
+client.run('token goes here')
---
+++ src/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, String, Integer, create_engine
+import os
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+connection_string = 'sqlite:///' + os.path.join(BASE_DIR, 'kbot.db')
+
+Base = declarative_base()
+Session = sessionmaker()
+
+engine = create_engine(connection_string, echo=True)
+
+class User(Base):
+    __tablename__ = 'Usr'
+    id = Column(Integer, primary_key=True)
+    user = Column(String(1000), unique=True)
+    user_lvl = Column(Integer)
