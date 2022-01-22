3bd4056da962b4d633acd83063e9d79354748988
Koala-Development <koala@mail.co.uk>
2022-01-22 17:18:20 +0000

    Fixed numerous issues relating to querying the database

--- src/main.py
+++ src/main.py
@@ -2,23 +2,23 @@ import discord
 from models import User, engine, Base, Session
 
 Base.metadata.create_all(engine)
-Session = Session()
+session = Session(bind=engine)
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
-        author = message.author
-        user = User.query.filter_by(user=author).first()
+        author = message.author.name + message.author.discriminator
+        user = session.query(User).filter(User.user==author).first()
         if user:
-            user.user_lvl =+ 5
+            user.user_lvl = user.user_lvl + 5
             if message.content.startswith('&&lvl'):
-                await message.reply("Your level is: " + str(user.user_lvl))
+                await message.reply("Your level is: " + str(user.user_lvl), mention_author=False)
         else:
-            new_user = User(user=author, author_lvl=5)
-            Session.add(new_user)
-            Session.commit()
+            new_user = User(user=author, user_lvl=5)
+            session.add(new_user)
+            session.commit()
 
 client = MyClient()
 client.run('token goes here')
